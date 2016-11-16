c6684243ddccb748c92060bc22663825e55de3d2
Scott Louvau <slouvau@hotmail.com>
2016-11-16 08:55:38 -0800

    - Updating Json.net version to 9.0.1. - Moving CommandLine to Arriba - Moving CommandLine tests, removing Arriba.Csv.Test. - Adding Arriba.Configure to set security (partial).

--- Arriba/Tools/Arriba.Configure/Properties/AssemblyInfo.cs
+++ Arriba/Tools/Arriba.Configure/Properties/AssemblyInfo.cs
@@ -1,19 +1,16 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Arriba.Csv.Test")]
+[assembly: AssemblyTitle("Arriba.Configure")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Arriba.Csv.Test")]
-[assembly: AssemblyCopyright("Copyright \u00A9  2016")]
+[assembly: AssemblyProduct("Arriba.Configure")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -23,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d3fd3d67-d4fc-4a23-883b-cc201cfe3c33")]
+[assembly: Guid("3745e7ba-1192-446c-ab4c-b15f6fe7035e")]
 
 // Version information for an assembly consists of the following four values:
 //
