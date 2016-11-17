b89fc2d637682b42f09bbeeb63050e71e6964e25
Scott Louvau <slouvau@hotmail.com>
2016-11-16 16:42:36 -0800

    - Adding 'BaseBlockResult' to include common components of other result types. - Ensuring Query returned in error results due to column access.

--- Arriba/Arriba/Model/Query/AggregationResult.cs
+++ Arriba/Arriba/Model/Query/AggregationResult.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using Arriba.Structures;
-
 namespace Arriba.Model.Query
 {
     /// <summary>
@@ -10,20 +8,11 @@ namespace Arriba.Model.Query
     ///  It contains a table with the aggregated values returned by each
     ///  dimension provided in the query.
     /// </summary>
-    public class AggregationResult : BaseResult
+    public class AggregationResult : BaseBlockResult
     {
-        /// <summary>
-        /// Total number of rows reflected in the results
-        /// </summary>
-        public uint Total { get; set; }
-        public AggregationQuery Query { get; set; }
-        public DataBlock Values { get; set; }
-
         internal object AggregationContext { get; set; }
 
-        public AggregationResult(AggregationQuery query)
-        {
-            this.Query = query;
-        }
+        public AggregationResult(AggregationQuery query) : base(query)
+        { }
     }
 }
