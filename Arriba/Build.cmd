9ab3e841a9e523e7b3fcabb087ad628184eacb97
Jeff King <jeffking+github@gmail.com>
2017-03-30 13:41:27 -0700

    Enabling build for VS 2017.

--- Arriba/Build.cmd
+++ Arriba/Build.cmd
@@ -1,6 +1,7 @@
 @ECHO OFF
 SETLOCAL ENABLEDELAYEDEXPANSION
-SET MsBuildPath="%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe"
+SET MsBuildPath=%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe
+IF NOT EXIST "%MsBuildPath%" SET MSBuildPath=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe
 
 @REM unsigned build
 %~dp0..\.nuget\NuGet.exe restore %~dp0Arriba.All.sln 
@@ -14,8 +15,8 @@ GOTO :EOF
 :Build
   ECHO.
   ECHO ======================================== %DATE% %TIME%
-  ECHO %MsBuildPath% %~dp0%~1 /p:Configuration=Release /p:Platform="Any CPU"
-  %MsBuildPath% %~dp0%~1 /p:Configuration=Release /p:Platform="Any CPU"
+  ECHO "%MsBuildPath%" %~dp0%~1 /p:Configuration=Release /p:Platform="Any CPU"
+  "%MsBuildPath%" %~dp0%~1 /p:Configuration=Release /p:Platform="Any CPU"
   ECHO ======================================== %DATE% %TIME%
   SET MsBuildError=!ERRORLEVEL!
   IF NOT "!MsBuildError!"=="0" GOTO Error
