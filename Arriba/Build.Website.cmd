4f6b7762894e523a28b1bcab3a360c938343e2a0
Scott Louvau (Design Laboratory) <v-scolo@microsoft.com>
2017-03-30 16:14:30 -0700

    Build.WebSite improvements.  - Fix Build.Website to clear a configuration folder if building without config (so the base site comes back).  - Make Build.Website push changes back to the source in case files are being updated in the Arriba copy instead of the correct location.  - Save configuration to configuration.BAK if a build happens to avoid losing user work.

--- Arriba/Build.Website.cmd
+++ Arriba/Build.Website.cmd
@@ -1,16 +1,17 @@
 @ECHO OFF
-SET BabelBuildPaths="%~dp0Arriba.Web\jsx"
 
 IF EXIST "%1" (
- ECHO - Importing Configuration...
- ROBOCOPY /E /XO /NJH /NJS /MIR "%1" "%~dp0Arriba.Web\configuration"
- SET BabelBuildPaths="%~dp0Arriba.Web\configuration" %BabelBuildPaths%
+  ECHO - Synchronizing Configuration...
+  ROBOCOPY /E /XO /NJH /NJS "%~dp0Arriba.Web\configuration" "%1"
+  ROBOCOPY /E /XO /NJH /NJS /MIR "%1" "%~dp0Arriba.Web\configuration"
+) ELSE (
+  IF EXIST "%~dp0Arriba.Web\configuration" (
+    IF EXIST "%~dp0Arriba.Web\configuration.BAK" RMDIR /S /Q "%~dp0Arriba.Web\configuration.BAK"
+    MOVE "%~dp0Arriba.Web\configuration" "%~dp0Arriba.Web\configuration.BAK"
+  )
 )
 
 ECHO - Building Website...
-
-pushd "%~dp0Arriba.Web
+PUSHD "%~dp0Arriba.Web
 CALL webpack
-popd
-
-:: Can add --no-comments --minified to reduce the bundle, but this version prefers easier debuggability.
+POPD
