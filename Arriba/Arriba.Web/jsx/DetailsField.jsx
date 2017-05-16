32b2319f5ac2ffa42b03f5028bb24bf2b4f07519
Scott Louvau <slouvau@hotmail.com>
2017-05-16 13:43:56 -0700

    CR comments

--- Arriba/Arriba.Web/jsx/DetailsField.jsx
+++ Arriba/Arriba.Web/jsx/DetailsField.jsx
@@ -1,25 +1,23 @@
 // Writes out a field with label and highlighting, but only if the value is set
 export default React.createClass({
-    urlReplace: function (urlFormat, marker, id) {
-        var idForUrl = encodeURIComponent(id);
-        return urlFormat.replace(new RegExp(marker), idForUrl);
-    },
     render: function () {
         if (!this.props.value || this.props.value === "N/A" || this.props.value === "Unknown") return null;
         
         var content = <span dangerouslySetInnerHTML={highlight(this.props.value)} title={this.props.columnName + ':' + stripHighlight(this.props.value)} />;
         if (this.props.bold) content = <b>{content}</b>;
 
-        var link = null;
-        if(this.props.url) content = <a target="_blank" href={this.urlReplace(this.urlReplace(this.props.url, "{ID}", this.props.id), "{Value}", this.props.value)} title={this.props.title}>{content}</a>;
+        if(this.props.url) {
+            var fullUrl = this.props.url.replace(/{ID}/g, encodeURIComponent(this.props.id)).replace(/{Value}/g, encodeURIComponent(this.props.value));
+            var target = (fullUrl.indexOf("mailto:") === 0 ? "_self" : "_blank");
+            content = <a target={target} title={this.props.title} href={fullUrl}>{content}</a>;
+        }
 
-        var label = null;
-        if (this.props.label) label = <span>{this.props.label + " "}</span>;
+        var label = this.props.label && <span>{this.props.label + " "}</span>
 
         if (this.props.inline === "true") {
-            return <span>{label} {content}{link}</span>
+            return <span>{label} {content}</span>
         } else {
-            return <div>{label} {content}{link}</div>
+            return <div>{label} {content}</div>
         }
     }
 });
