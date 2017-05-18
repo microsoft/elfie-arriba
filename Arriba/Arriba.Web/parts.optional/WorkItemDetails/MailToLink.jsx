04f04e5f5de456b3f10e37998fe773aa0a900157
Scott Louvau <slouvau@hotmail.com>
2017-05-18 14:30:16 -0700

    Fix MailToLink highlight. Make history entry who links.

--- Arriba/Arriba.Web/parts.optional/WorkItemDetails/MailToLink.jsx
+++ Arriba/Arriba.Web/parts.optional/WorkItemDetails/MailToLink.jsx
@@ -4,7 +4,7 @@ import DetailsField from "../../parts/DetailsField";
 export default React.createClass({
     render: function () {
         var linkToItem = document.location.protocol + "//" + document.location.host + document.location.pathname + "?t=" + encodeURIComponent(this.props.table) + "&q=ID%3D" + encodeURIComponent(this.props.itemId) + "&open=" + encodeURIComponent(this.props.itemId);
-        var mailTo = "mailto:" + encodeURIComponent(this.props.value) + "?subject=" + encodeURIComponent(this.props.table + "." + this.props.itemId) + "&body=" + encodeURIComponent(linkToItem);
+        var mailTo = "mailto:" + encodeURIComponent(stripHighlight(this.props.value)) + "?subject=" + encodeURIComponent(this.props.table + "." + this.props.itemId) + "&body=" + encodeURIComponent(linkToItem);
         return <DetailsField value={this.props.value} columnName={this.props.columnName} label={this.props.columnName} bold="true" url={mailTo} />;   
     }
 });
