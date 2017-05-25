a90c8e9b2c3eb742d8250c01d672623ecf0c67ba
Jeff King <jeffking+github@gmail.com>
2017-05-25 14:17:17 -0700

    String.prototype.endsWith polyfill for IE.

--- Arriba/Arriba.Web/js/utilities.js
+++ Arriba/Arriba.Web/js/utilities.js
@@ -54,6 +54,16 @@ String.prototype.startsWith = String.prototype.startsWith || function(searchStri
     return this.indexOf(searchString, position) === position;
 };
 
+String.prototype.endsWith = String.prototype.endsWith || function(searchString, position) {
+    var subjectString = this.toString();
+    if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
+        position = subjectString.length;
+    }
+    position -= searchString.length;
+    var lastIndex = subjectString.lastIndexOf(searchString, position);
+    return lastIndex !== -1 && lastIndex === position;
+};
+
 String.prototype.trimIf = function(prefix) {
     return this.startsWith(prefix)
         ? this.substring(prefix.length)
