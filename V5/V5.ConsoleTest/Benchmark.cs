9ebcd67082ca6ec7f1fdd3a8d03c3e9c46eadc17
Scott Louvau <slouvau@hotmail.com>
2017-06-05 16:12:50 -0700

    Added V5 native, testing string instruction filtering.

---
+++ V5/V5.ConsoleTest/Benchmark.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace V5.ConsoleTest
+{
+    public static class Benchmark
+    {
+        public static void Compare(string description, int iterations, long bytes, string[] variantNames, params Action[] variants)
+        {
+            Console.WriteLine("{0}: {1:n0} iterations of {2:n0} bytes", description, iterations, bytes);
+
+            double benchmarkMilliseconds = 0.0;
+
+            for(int i = 0; i < variants.Length; ++i)
+            {
+                Action variant = variants[i];
+
+                Stopwatch w = Stopwatch.StartNew();
+                for (int iteration = 0; iteration < iterations; ++iteration)
+                {
+                    variant();
+                }
+                w.Stop();
+
+                if (i == 0) benchmarkMilliseconds = w.ElapsedMilliseconds;
+
+                double totalBytes = bytes * (long)iterations;
+                double gigabytesPerSecond = ((double)totalBytes / (double)(1024 * 1024 * 1024)) / ((double)w.ElapsedMilliseconds / (double)1000);
+                double relativePerformance = benchmarkMilliseconds / (double)w.ElapsedMilliseconds;
+                Console.WriteLine(" - {0}: {1:n0}ms, {2:n2} GB/s [{3:n1}x]", variantNames[i], w.ElapsedMilliseconds, gigabytesPerSecond, relativePerformance);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
--- V5/V5.ConsoleTest/Program.cs
+++ V5/V5.ConsoleTest/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace V5.ConsoleTest
 {
@@ -10,6 +6,36 @@ namespace V5.ConsoleTest
     {
         static void Main(string[] args)
         {
+            int length = 8000000;
+            byte[] test = new byte[length];
+
+            Random r = new Random();
+            r.NextBytes(test);
+
+            byte rangeStart = 0;
+            byte rangeEnd = 4;
+
+            int rm = CountManaged(test, rangeStart, rangeEnd);
+            int rn = String8Native.CountInRange(test, rangeStart, rangeEnd);
+            Console.WriteLine($"Managed: {rm:n0}, Native: {rn:n0}");
+
+            Benchmark.Compare("Count bytes in range", 100, length, new string[] { "Managed", "Native" },
+                () => CountManaged(test, rangeStart, rangeEnd),
+                () => String8Native.CountInRange(test, rangeStart, rangeEnd)
+            );
+        }
+
+        private static int CountManaged(byte[] test, byte rangeStart, byte rangeEnd)
+        {
+            int count = 0;
+
+            for (int i = 0; i < test.Length; ++i)
+            {
+                byte value = test[i];
+                if (value >= rangeStart && value <= rangeEnd) count++;
+            }
+
+            return count;
         }
     }
 }
