91915120bb602aaf20a77acd2ff58d72d010c0e6
Jeff King <jeffking+github@gmail.com>
2017-06-06 13:27:56 -0700

    Refactor Suggestions away from SearchBox.

--- Arriba/Arriba.Web/parts/SearchBox.jsx
+++ Arriba/Arriba.Web/parts/SearchBox.jsx
@@ -1,10 +1,11 @@
 import "./SearchBox.scss";
 import EventedComponent from "./EventedComponent";
+import Suggestions from "./Suggestions";
 
 export default class SearchBox extends EventedComponent {
     constructor(props) {
         super(props);
-        this.state = { suggestions: [], sel: 0, completed: "", completionCharacters: [] };
+        this.state = {};
         this.events = {
             "storage": e => {
                 if (!["favorites"].includes(e.key)) return;
@@ -12,13 +13,9 @@ export default class SearchBox extends EventedComponent {
             },
             "click": e => { // Ideally use input.blur, but that fires before suggestions.item.onClick.
                 if (e.target === this.refs.input) return;
-                this.clear();
+                this.refs.suggestions.clear();
             },
-            "mousewheel": e => { // Prefer "mousewheel" over "scroll" as the latter gets (noisily) triggered by results loading.
-                if (!this.refs.suggestions || this.refs.suggestions.contains(e.target)) return;
-                this.clear();
-            },
-        }
+        };
     }
     componentDidMount() {
         super.componentDidMount();
@@ -28,116 +25,36 @@ export default class SearchBox extends EventedComponent {
         // IE focus/blur spurriously triggers onInput(), this works around that.
         if (this.props.query !== query) this.props.queryChanged(query);
     }
-    onKeyDown(e) {
-        if (!this.state.suggestions.length) return;
-        if (e.key === "ArrowUp") {
-            this.setState({ sel: (this.state.sel - 1).clamp(0, this.state.suggestions.length - 1) });
-            e.stopPropagation();
-            e.preventDefault(); // Prevent cursor moving to beginning of line.
-        }
-        if (e.key === "ArrowDown") {
-            this.setState({ sel: (this.state.sel + 1).clamp(0, this.state.suggestions.length - 1) });
-            e.stopPropagation();
-        }
-        if (e.key === "Enter" || this.state.completionCharacters.includes(e.key)) {
-            var item = this.state.suggestions[this.state.sel];
-
-            var separator = (item.category === "Value" && e.key !== " " ? "" : " ");
-            var suffix = (e.key === "Enter" || e.key === "Tab" || e.key === " ") ? "" : e.key;
-            var newQuery = item.replaceAs || (this.state.completed + item.completeAs + separator + suffix);
-            this.props.queryChanged(newQuery);
-            e.preventDefault(); // Suppress focus tabbing.
-        }
-        if (e.key === "Escape") {
-            this.clear();
-        }
-    }
-    onClickSuggestion(item) {
-        var separator = (item.category === "Value" ? "" : " ");
-        this.props.queryChanged(item.replaceAs || this.state.completed + item.completeAs + separator);
-        this.refs.input.focus();
-    }
-    fetch() {
-        if (this.lastRequest) this.lastRequest.abort();
-        this.lastRequest = jsonQuery(
-            configuration.url + "/suggest?q=" + encodeURIComponent(this.props.query),
-            data => {
-                this.setState({
-                    suggestions: data.content.suggestions,
-                    sel: 0,
-                    completed: data.content.complete, 
-                    completionCharacters: data.content.completionCharacters.map(c => ({ "\t": "Tab" })[c] || c),
-                });
-            }
-        );
-    }
-    clear() {
-        this.setState({ suggestions: [] });
-    }
     toggleFavorite() {
         if (!this.props.parsedQuery) return;
         localStorage.updateJson("favorites", favs => (favs || []).toggle(this.props.parsedQuery));
     }
     render() {
-        // Generates a SVG histogram to be displayed behind the completion list.
-        // The path goes counter-clockwise starting from the top-right.
-        var svg = this.state.suggestions.length && (() => {
-            var d = '';
-
-            // The inst() currently concats SVG commands to the list 'd'.
-            // However when debugging, it is useful to redirect the ...params to the console.
-            const inst = (...params) => d += params.join(" ") + " ";
-
-            // Scrape ___% from the item.hint. If not found, default to 0.
-            const values = this.state.suggestions.map(item => new Number(item.hint.replace('%', '')) + 0 || 0);
-
-            const w = 80; // Matches CSS declared width.
-            inst("M", w, 0);
-            inst("L", w - values[0] * 0.75, 0);
-            const max = Math.max(...values) || 1; // Prevent divide by zero.
-            var y = 0; // Running total fo the height.
-            values.forEach(val => {
-                const x = w - (val/max) * w;
-                inst("S", x, y, ",", x, y + 18);
-                y += 37; // Matches the CSS declared height of each row.
-            });
-            const x = w - values[values.length - 1] * 0.75;
-            inst("S", x, y, x, y + 18);
-            inst("L", w, y);
-            inst("Z");
-            return <svg><path id="p" d={d} /></svg>
-        })();
-
+        var star = (localStorage.getJson("favorites") || []).includes(this.props.parsedQuery) ? "icon-solid-star" : "icon-outlined-star"
         return <div className="searchBox">
             <div className={ "loading " + (this.props.loading ? "loading-active" : "") }></div>
             <input ref="input" type="text"
                 spellCheck="false"
-                placeholder="Search for..."
-                tabIndex="1" 
+                placeholder="Search for..." 
+                tabIndex="1"
                 value={this.props.query}
+                onClick={e => this.refs.suggestions.fetch()}
                 onInput={e => this.onInput(e.target.value)}
-                onKeyDown={e => this.onKeyDown(e)}
-                onClick={e => this.fetch()} />
+                onKeyDown={e => this.refs.suggestions.onKeyDown(e)} />
             <div className="rail">
                 {this.state.completed}
                 <span style={{ position: "relative" }} >
                     <div className="railContents">
-                        {this.state.suggestions.length > 0 &&
-                            <div ref="suggestions" className="suggestions" style={isIE() ? {minWidth: "300px"} : {}} >
-                                {svg}
-                                {this.state.suggestions.map((item, index) =>
-                                    <div className={"suggestion " + (this.state.sel == index ? "suggestion-sel" : "" )}
-                                        onClick={e => this.onClickSuggestion(item) }>
-                                        <span>{item.display}</span>
-                                        <span className="suggestion-hint">{item.hint}</span>
-                                    </div>
-                                )}
-                            </div>
-                        }
+                        <Suggestions
+                            ref="suggestions"
+                            query={this.props.query}
+                            queryChanged={q => this.props.queryChanged(q)}
+                            completedChanged={c => this.setState({ completed: c })}
+                            refocus={() => this.refs.input.focus()} />
                     </div>
                 </span>
             </div>
-            <i className={"searchIcon clickable " + ((localStorage.getJson("favorites") || []).includes(this.props.parsedQuery) ? "icon-solid-star" : "icon-outlined-star")} onClick={e => this.toggleFavorite()}></i>
+            <i className={"searchIcon clickable " + star} onClick={e => this.toggleFavorite(e)}></i>
             <i className="searchIcon icon-find"></i>
         </div>
     }
---
+++ Arriba/Arriba.Web/parts/Suggestions.jsx
@@ -0,0 +1,111 @@
+import "./SearchBox.scss";
+import EventedComponent from "./EventedComponent";
+
+export default class Suggestions extends EventedComponent {
+    constructor(props) {
+        super(props);
+        this.state = { suggestions: [], sel: 0, completed: "", completionCharacters: [] };
+        this.events = {
+            "mousewheel": e => { // Prefer "mousewheel" over "scroll" as the latter gets (noisily) triggered by results loading.
+                if (!this.refs.suggestions || this.refs.suggestions.contains(e.target)) return;
+                this.clear();
+            },
+        };
+    }
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.query !== this.props.query) this.fetch();
+    }
+
+    fetch() {
+        if (this.lastRequest) this.lastRequest.abort();
+        this.lastRequest = jsonQuery(
+            configuration.url + "/suggest?q=" + encodeURIComponent(this.props.query),
+            data => {
+                this.props.completedChanged(data.content.complete);
+                this.setState({
+                    suggestions: data.content.suggestions,
+                    sel: 0,
+                    completed: data.content.complete, 
+                    completionCharacters: data.content.completionCharacters.map(c => ({ "\t": "Tab" })[c] || c),
+                });
+            }
+        );
+    }
+    clear() {
+        this.setState({ suggestions: [] });
+    }
+
+    onClick(item) {
+        var separator = (item.category === "Value" ? "" : " ");
+        this.props.queryChanged(this.state.completed + item.completeAs + separator);
+        this.props.refocus();
+    }
+    onKeyDown(e) {
+        if (!this.state.suggestions.length) return;
+        if (e.key === "ArrowUp") {
+            this.setState({ sel: (this.state.sel - 1).clamp(0, this.state.suggestions.length - 1) });
+            e.stopPropagation();
+            e.preventDefault(); // Prevent cursor moving to beginning of line.
+        }
+        if (e.key === "ArrowDown") {
+            this.setState({ sel: (this.state.sel + 1).clamp(0, this.state.suggestions.length - 1) });
+            e.stopPropagation();
+        }
+        if (e.key === "Enter" || this.state.completionCharacters.includes(e.key)) {
+            var item = this.state.suggestions[this.state.sel];
+
+            var separator = (item.category === "Value" && e.key !== " " ? "" : " ");
+            var suffix = (e.key === "Enter" || e.key === "Tab" || e.key === " ") ? "" : e.key;
+            var newQuery = this.state.completed + item.completeAs + separator + suffix;
+            this.props.queryChanged(newQuery);
+            e.preventDefault(); // Suppress focus tabbing.
+        }
+        if (e.key === "Escape") {
+            this.clear();
+        }
+    }
+
+    get svg() {
+        if (!this.state.suggestions.length) return false;
+
+        // Generates a SVG histogram to be displayed behind the completion list.
+        // The path goes counter-clockwise starting from the top-right.
+        var d = '';
+
+        // The inst() currently concats SVG commands to the list 'd'.
+        // However when debugging, it is useful to redirect the ...params to the console.
+        const inst = (...params) => d += params.join(" ") + " ";
+
+        // Scrape ___% from the item.hint. If not found, default to 0.
+        const values = this.state.suggestions.map(item => new Number(item.hint.replace('%', '')) + 0 || 0);
+
+        const w = 80; // Matches CSS declared width.
+        inst("M", w, 0);
+        inst("L", w - values[0] * 0.75, 0);
+        const max = Math.max(...values) || 1; // Prevent divide by zero.
+        var y = 0; // Running total fo the height.
+        values.forEach(val => {
+            const x = w - (val/max) * w;
+            inst("S", x, y, ",", x, y + 18);
+            y += 37; // Matches the CSS declared height of each row.
+        });
+        const x = w - values[values.length - 1] * 0.75;
+        inst("S", x, y, x, y + 18);
+        inst("L", w, y);
+        inst("Z");
+        return <svg><path id="p" d={d} /></svg>
+    }
+    render() {
+        return this.state.suggestions.length > 0 &&
+            <div ref="suggestions" className="suggestions" style={isIE() ? {minWidth: "300px"} : {}} >
+                {this.svg}
+                {this.state.suggestions.map((item, index) =>
+                    <div className={"suggestion " + (this.state.sel == index ? "suggestion-sel" : "" )}
+                        onClick={e => this.onClick(item) }>
+                        <span>{item.display}</span>
+                        <span className="suggestion-hint">{item.hint}</span>
+                    </div>
+                )}
+            </div>
+    }
+}
