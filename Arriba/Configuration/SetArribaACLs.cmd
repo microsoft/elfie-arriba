c3aac7fa3bc54e813488d984f213764c9fcbee62
Scott Louvau <slouvau@hotmail.com>
2017-06-19 09:15:26 -0700

    Improvements to make Bung Walkthrough path work better.

--- Arriba/Configuration/SetArribaACLs.cmd
+++ Arriba/Configuration/SetArribaACLs.cmd
@@ -1,12 +1,15 @@
 @ECHO OFF
 PUSHD %~dp0..
+SET ServiceAccount=%1
 
-:: The Arriba service runs as an App Pool account, which has read/execute access to the Arriba site, DiskCache, and read-write access to the Arriba Logs.
-:: Users browsing access the site as themselves (Windows Auth). Everyone needs read/execute access to the Arriba.Web and redirect site only.
+:: The Arriba service runs as an App Pool account, which needs read-write access to DiskCache and Logs.
+:: Users access the site as themselves, due to Windows Authentication, so Everyone needs read/execute access to Arriba.IIS, Arriba.Web, and Redirect.
+:: If Tables are written directly only (not imported to the Arriba HTTP service), the DiskCache folder can be read only for IIS_IUSRS for extra security.
 
 ECHO - DiskCache [Arriba Data]
 IF NOT EXIST DiskCache (MD DiskCache)
-ICACLS DiskCache /Grant IIS_IUSRS:(OI)(CI)(RX)
+ICACLS DiskCache /Grant IIS_IUSRS:(OI)(CI)(F)
+IF NOT "%1"=="" ICACLS DiskCache /Grant %1:(OI)(CI)(F)
 ICACLS DiskCache\* /Reset /T
 
 ECHO - Arriba.IIS
