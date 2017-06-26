944543e84ab51aa12b205d87e045e2d42662285d
Scott Louvau <slouvau@hotmail.com>
2017-06-26 16:00:54 -0700

    V5: Starting BinaryReaderWriter porting

---
+++ V5/V5.Native/BinaryReaderWriterExtensions.cpp
@@ -0,0 +1,7 @@
+#include "stdafx.h"
+#include "BinaryReaderWriterExtensions.h"
+
+
+BinaryReaderWriterExtensions::BinaryReaderWriterExtensions()
+{
+}
---
+++ V5/V5.Native/BinaryReaderWriterExtensions.h
@@ -0,0 +1,7 @@
+#pragma once
+ref class BinaryReaderWriterExtensions
+{
+public:
+	BinaryReaderWriterExtensions();
+};
+
