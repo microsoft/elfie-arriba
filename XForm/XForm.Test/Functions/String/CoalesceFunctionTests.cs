277dbd42756806dfed97e74af8c21b46fb6ba584
Ryley Taketa <rtaket@microsoft.com>
2018-01-03 07:07:37 -0800

    Add coalesce function.

---
+++ XForm/XForm.Test/Functions/String/CoalesceFunctionTests.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.CodeAnalysis.Elfie.Model.Strings;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XForm.Data;
+using XForm.Functions.String;
+
+namespace XForm.Test.Functions.String
+{
+    [TestClass]
+    public class CoalesceFunctionTests
+    {
+        [TestMethod]
+        public void Coalesce()
+        {
+            // Arrange
+
+            String8[] transformedArray = null;
+            bool[] nullArray = null;
+
+            // 1 a !   =   1
+            // 2 b -   =   2
+            // 3 - -   =   3 
+            // - d $   =   d
+            // - e -   =   e
+            // - - ^   =   ^
+            // - - -   =   -
+
+
+            String8[] column1 = new String8[]
+            {
+                "1".ToString8(),
+                "2".ToString8(),
+                "3".ToString8(),
+                String8.Empty,
+                String8.Empty,
+                String8.Empty,
+                String8.Empty,
+            };
+
+            String8[] column2 = new String8[]
+            {
+                "a".ToString8(),
+                "b".ToString8(),
+                String8.Empty,
+                "d".ToString8(),
+                "e".ToString8(),
+                String8.Empty,
+                String8.Empty,
+            };
+
+            String8[] column3 = new String8[]
+            {
+                "!".ToString8(),
+                String8.Empty,
+                String8.Empty,
+                "$".ToString8(),
+                String8.Empty,
+                "^".ToString8(),
+                String8.Empty,
+            };
+
+            String8[] expected = new String8[]
+            {
+                "1".ToString8(),
+                "2".ToString8(),
+                "3".ToString8(),
+                "d".ToString8(),
+                "e".ToString8(),
+                "^".ToString8(),
+                String8.Empty,
+            };
+
+
+            // Act
+            DataBatch actual = CoalesceColumn.CoalesceBatch(ref transformedArray, ref nullArray, DataBatch.All(column1), DataBatch.All(column2), DataBatch.All(column3));
+
+
+            // Assert
+            Assert.AreEqual(expected.Length, actual.Count, "The coalesced column length did not match the expected column length.");
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual.Array.GetValue(i));
+            }
+        }
+    }
+
+    static class String8Extensions
+    {
+        public static String8 ToString8(this string text)
+        {
+            return String8.Convert(text, new byte[String8.GetLength(text)]);
+        }
+    }
+}
---
+++ XForm/XForm/Functions/String/Coalesce.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.CodeAnalysis.Elfie.Model.Strings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using XForm.Commands;
+using XForm.Data;
+using XForm.Types;
+
+namespace XForm.Functions.String
+{
+    internal class CoalesceBuilder : IFunctionBuilder
+    {
+        public string Name => "Coalesce";
+        public string Usage => "Coalesce([ColumnName], [...n])";
+
+        public IDataBatchColumn Build(IDataBatchEnumerator source, WorkflowContext context)
+        {
+            List<IDataBatchColumn> columns = new List<IDataBatchColumn>();
+
+            while (context.Parser.HasAnotherArgument)
+            {
+                columns.Add(context.Parser.NextColumn(source, context));
+            }
+
+            if (columns.Count < 2)
+            {
+                throw new ArgumentOutOfRangeException("Coalesce requries at least two input columns.");
+            }
+
+            return CoalesceColumn.Build(
+                source,
+                columns);
+        }
+    }
+
+    public class CoalesceColumn : IDataBatchColumn
+    {
+        private IEnumerable<IDataBatchColumn> Columns { get; }
+
+        private CoalesceColumn(IEnumerable<IDataBatchColumn> columns)
+        {
+            Columns = columns;
+
+            ColumnDetails = new ColumnDetails("Coalesce", typeof(String8), true);
+        }
+
+        public static IDataBatchColumn Build(IDataBatchEnumerator source, IEnumerable<IDataBatchColumn> columns)
+        {
+            // Coalesce only works on String8 columns
+            if (columns.Any(column => column.ColumnDetails.Type != typeof(String8)))
+            {
+                throw new ArgumentOutOfRangeException("Every input columns for Coalesce must be of type String8.");
+            }
+
+            return new CoalesceColumn(columns);
+        }
+
+        public ColumnDetails ColumnDetails
+        {
+            get;
+        }
+
+        public Func<DataBatch> Getter()
+        {
+            String8[] transformedArray = null;
+            bool[] nullArray = null;
+
+            return () => CoalesceColumn.CoalesceBatch(ref transformedArray, ref nullArray, Columns.Select(column => column.Getter()()).ToArray());
+        }
+
+        public static DataBatch CoalesceBatch(ref String8[] transformedArray, ref bool[] nullArray, params DataBatch[] batches)
+        {
+            Allocator.AllocateToSize(ref transformedArray, batches[0].Count);
+            Allocator.AllocateToSize(ref nullArray, batches[0].Count);
+
+            for (int i = 0; i < batches[0].Count; ++i)
+            {
+                foreach (var column in batches)
+                {
+                    // Choose the first non-null/empty value
+                    if ((column.IsNull != null && !column.IsNull[i]) || (String8)column.Array.GetValue(i) != String8.Empty)
+                    {
+                        transformedArray[i] = (String8)column.Array.GetValue(i);
+                        break;
+                    }
+                }
+
+                // Update the null array if the value is null/empty
+                if (transformedArray[i] == String8.Empty)
+                {
+                    nullArray[i] = true;
+                }
+            }
+
+            return DataBatch.All(transformedArray, batches[0].Count, nullArray);
+        }
+    }
+}
