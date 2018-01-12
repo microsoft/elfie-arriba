22229dc1ae8a279e384197c997efb157204fe831
Jeff King <jeffking+github@gmail.com>
2018-01-11 18:23:08 -0800

    Fix button style for schema.

--- XForm/XForm.Web/Index.jsx
+++ XForm/XForm.Web/Index.jsx
@@ -158,8 +158,8 @@ class Index extends React.Component {
             <div id="schema">
                 <div className="schemaHeader">
                     {!this.state.userCols.length && this.state.schemaBody && <span>{this.state.schemaBody.length} Columns</span>}
-                    {!!this.state.userCols.length && <span className="schemaButton" onClick={e => this.setState({ userCols: [] }, () => this.queryChanged())}>Reset</span>}
-                    {!!this.state.userCols.length && <span className="schemaButton" onClick={e => {
+                    {!!this.state.userCols.length && <span className="button" onClick={e => this.setState({ userCols: [] }, () => this.queryChanged())}>Reset</span>}
+                    {!!this.state.userCols.length && <span className="button" onClick={e => {
                         const newLine = this.query.endsWith('\n') ? '' : '\n'
                         const userCols = this.state.userCols.length && `${newLine}select ${this.state.userCols.map(c => `[${c}]`).join(' ')}` || ''
                         const r = this.editor.getModel().getFullModelRange()
