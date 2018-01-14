8c3a4d90474e606f9b2bf667713a3cafbe5964db
Scott Louvau <v-scolo@microsoft.com>
2018-01-14 14:22:58 -0800

    XForm: Where request count optimization   - Where can request more than desiredCount.   - Where returns paged results if it has too many.   - Where tracks the number of rows per successful match to request an optimized number of rows.
    These changes prevent Where from slowing to one at a time when a limit query afterward is close to the limit, and allows where to scale the request count more intelligently as the match rate is determined.

--- XForm/XForm/Verbs/Where.cs
+++ XForm/XForm/Verbs/Where.cs
@@ -27,12 +27,25 @@ namespace XForm.Verbs
         private BitVector _vector;
         private RowRemapper _mapper;
 
+        // Keep current filtered DataBatches from the source, to allow requesting more than the desired count
+        private DataBatch[] _currentBatches;
+        private int _currentMatchesTotal;
+        private int _currentMatchesReturned;
+        private int _nextCountToReturn;
+
+        // Track the total rows we've gotten and returned
+        private int _totalRowsRetrieved;
+        private int _totalRowsMatched;
+
         public Where(IDataBatchEnumerator source, IExpression expression) : base(source)
         {
             _expression = expression;
 
             // Build a mapper to hold matching rows and remap source batches
             _mapper = new RowRemapper();
+
+            // Allocate room to cache returned DataBatches
+            _currentBatches = new DataBatch[source.Columns.Count];
         }
 
         public override Func<DataBatch> ColumnGetter(int columnIndex)
@@ -45,27 +58,77 @@ namespace XForm.Verbs
 
             return () =>
             {
-                // Get the batch from the source for this column
-                DataBatch batch = getter();
+                // If we're done returning from the previous batch, ...
+                if (_currentMatchesReturned == 0)
+                {
+                    // Get the batch from the source for this column
+                    DataBatch batch = getter();
 
-                // Remap the DataBatch indices for this column for the rows which matched the clause
-                return _mapper.Remap(batch, ref remapArray);
+                    // Remap the DataBatch indices for this column for the rows which matched the clause
+                    _currentBatches[columnIndex] = _mapper.Remap(batch, ref remapArray);
+                }
+
+                return _currentBatches[columnIndex].Slice(_currentMatchesReturned, _nextCountToReturn);
             };
+        }
+
+        private int CountToRequest(int desiredCount)
+        {
+            // By default, request the amount to return
+            int result = desiredCount;
+
+            // Request enough to get the desired match count, if we know the match percentage so far
+            if(_totalRowsMatched > 0)
+            {
+                double rowsPerMatch = (double)_totalRowsRetrieved / (double)_totalRowsMatched;
+                result = (int)(desiredCount * rowsPerMatch);
+            }
+
+            // Never request more than 16x the match count
+            if (result > desiredCount * 16) result = desiredCount * 16;
+
+            // Always request at least 100 rows
+            if (result < 100) result = 100;
+
+            return result;
         }
 
         public override int Next(int desiredCount)
         {
-            int outerCount;
-            while ((outerCount = _source.Next(desiredCount)) > 0)
+            _currentMatchesReturned += _nextCountToReturn;
+
+            // If we previously retrieved extra rows, return more of those
+            if (_currentMatchesTotal > _currentMatchesReturned)
             {
+                _nextCountToReturn = Math.Min(desiredCount, _currentMatchesTotal - _currentMatchesReturned);
+                return _nextCountToReturn;
+            }
+
+            // Otherwise, retrieve more rows
+            int countToRequest = CountToRequest(desiredCount);
+            _currentMatchesReturned = 0;
+
+            int outerCount;
+            while ((outerCount = _source.Next(countToRequest)) > 0)
+            {
+                // Track the total retrieved from the source
+                _totalRowsRetrieved += outerCount;
+
                 Allocator.AllocateToSize(ref _vector, outerCount);
                 _vector.None();
-                _expression.Evaluate(_vector);
 
-                // Stop if we got rows, otherwise get the next source batch
+                // Match the query expression and count matches
+                _expression.Evaluate(_vector);
                 _mapper.SetMatches(_vector);
-                int count = _mapper.Count;
-                if (count > 0) return count;
+                _currentMatchesTotal = _mapper.Count;
+                _totalRowsMatched += _currentMatchesTotal;
+
+                // If we got matches, return the first set requested, otherwise ask for more
+                if (_currentMatchesTotal > 0)
+                {
+                    _nextCountToReturn = Math.Min(desiredCount, _currentMatchesTotal - _currentMatchesReturned);
+                    return _nextCountToReturn;
+                }
             }
 
             return 0;
