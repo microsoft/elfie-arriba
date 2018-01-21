a63386028879bd59497707c89e0247d7195666e6
Scott Louvau <v-scolo@microsoft.com>
2018-01-20 22:59:42 -0800

    XForm: Choose fixed.

--- XForm/XForm/Verbs/Choose.cs
+++ XForm/XForm/Verbs/Choose.cs
@@ -7,149 +7,140 @@ using System.Linq;
 
 using XForm.Data;
 using XForm.Extensions;
+using XForm.Functions;
 using XForm.Query;
 using XForm.Transforms;
 
 namespace XForm.Verbs
 {
-    //internal class ChooseBuilder : IVerbBuilder
-    //{
-    //    public string Verb => "choose";
-    //    public string Usage => "choose {Max|Min} {RankColumn} {IdentityColumns}";
+    internal class ChooseBuilder : IVerbBuilder
+    {
+        public string Verb => "choose";
+        public string Usage => "choose {Max|Min} {RankColumn} {IdentityColumns}";
 
-    //    public IXTable Build(IXTable source, XDatabaseContext context)
-    //    {
-    //        ChooseDirection direction = context.Parser.NextEnum<ChooseDirection>();
-    //        IXColumn rankColumn = context.Parser.NextColumn(source, context);
+        public IXTable Build(IXTable source, XDatabaseContext context)
+        {
+            ChooseDirection direction = context.Parser.NextEnum<ChooseDirection>();
+            IXColumn rankColumn = context.Parser.NextColumn(source, context);
 
-    //        List<IXColumn> identityColumns = new List<IXColumn>();
-    //        do
-    //        {
-    //            identityColumns.Add(context.Parser.NextColumn(source, context));
-    //        } while (context.Parser.HasAnotherPart);
+            List<IXColumn> identityColumns = new List<IXColumn>();
+            do
+            {
+                identityColumns.Add(context.Parser.NextColumn(source, context));
+            } while (context.Parser.HasAnotherPart);
 
-    //        return new Choose(source, direction, rankColumn, identityColumns);
-    //    }
-    //}
+            return new Choose(source, direction, rankColumn, identityColumns);
+        }
+    }
 
-    //public class Choose : IXTable
-    //{
-    //    private IXTable _source;
+    public class Choose : IXTable
+    {
+        private IXTable _source;
+        private IXColumn[] _columns;
 
-    //    private RowRemapper _chosenRowsFilter;
-    //    private ChooseDictionary _dictionary;
-    //    private bool _isDictionaryBuilt;
-    //    private int _totalRowsRead;
+        private RowRemapper _chosenRowsFilter;
+        private ChooseDictionary _dictionary;
+        private bool _isDictionaryBuilt;
+        private int _totalRowsRead;
 
-    //    private Func<XArray>[] _keyColumnGetters;
-    //    private Func<XArray> _rankColumnGetter;
+        private Func<XArray>[] _keyColumnGetters;
+        private Func<XArray> _rankColumnGetter;
 
-    //    public Choose(IXTable source, ChooseDirection direction, IXColumn rankColumn, IList<IXColumn> keyColumns)
-    //    {
-    //        _source = source;
+        public Choose(IXTable source, ChooseDirection direction, IXColumn rankColumn, IList<IXColumn> keyColumns)
+        {
+            _source = source;
 
-    //        // Build a typed dictionary to handle the rank and key column types
-    //        _dictionary = new ChooseDictionary(direction, rankColumn.ColumnDetails, keyColumns.Select((col) => col.ColumnDetails).ToArray());
+            // Build a typed dictionary to handle the rank and key column types
+            _dictionary = new ChooseDictionary(direction, rankColumn.ColumnDetails, keyColumns.Select((col) => col.ColumnDetails).ToArray());
 
-    //        // Retrieve the getters for all columns
-    //        _rankColumnGetter = rankColumn.Getter();
-    //        _keyColumnGetters = keyColumns.Select((col) => col.Getter()).ToArray();
+            // Retrieve the getters for all columns
+            _rankColumnGetter = rankColumn.CurrentGetter();
+            _keyColumnGetters = keyColumns.Select((col) => col.CurrentGetter()).ToArray();
 
-    //        _chosenRowsFilter = new RowRemapper();
-    //    }
+            _chosenRowsFilter = new RowRemapper();
 
-    //    public IReadOnlyList<IXColumn> Columns => _source.Columns;
+            _columns = source.Columns.Select((col) => new RemappedColumn(this, col, _chosenRowsFilter)).ToArray();
+        }
 
-    //    public int CurrentRowCount { get; private set; }
+        public IReadOnlyList<IXColumn> Columns => _columns;
 
-    //    public Func<XArray> ColumnGetter(int columnIndex)
-    //    {
-    //        // Get the source getter
-    //        Func<XArray> sourceGetter = _source.ColumnGetter(columnIndex);
+        public int CurrentRowCount { get; private set; }
+        public ArraySelector CurrentSelector => ArraySelector.All(_totalRowsRead).Slice(_totalRowsRead - CurrentRowCount, CurrentRowCount);
 
-    //        // Cache an array to remap rows which were chosen
-    //        int[] remapArray = null;
+        public int Next(int desiredCount)
+        {
+            // If this is the first call, walk all rows once to find best rows
+            if (!_isDictionaryBuilt)
+            {
+                _isDictionaryBuilt = true;
+                BuildChooseDictionary();
+                _source.Reset();
+            }
 
-    //        return () =>
-    //        {
-    //            // Get the source values for this xarray
-    //            XArray xarray = sourceGetter();
+            int outerCount;
+            while ((outerCount = _source.Next(desiredCount)) > 0)
+            {
+                // Ask for the indices of rows which were chosen in this page
+                XArray chosenRows = _dictionary.GetChosenRows(_totalRowsRead, _totalRowsRead + outerCount, _totalRowsRead);
 
-    //            // Remap to just the rows chosen
-    //            return _chosenRowsFilter.Remap(xarray, ref remapArray);
-    //        };
-    //    }
+                // Track the total row count (so we know which rows to ask for chosens each time)
+                _totalRowsRead += outerCount;
 
-    //    public int Next(int desiredCount)
-    //    {
-    //        // If this is the first call, walk all rows once to find best rows
-    //        if (!_isDictionaryBuilt)
-    //        {
-    //            _isDictionaryBuilt = true;
-    //            BuildChooseDictionary();
-    //            _source.Reset();
-    //        }
+                // Tell the remapper to filter to chosen rows
+                _chosenRowsFilter.SetMatches((int[])chosenRows.Array, chosenRows.Count);
 
-    //        int outerCount;
-    //        while ((outerCount = _source.Next(desiredCount)) > 0)
-    //        {
-    //            // Ask for the indices of rows which were chosen in this page
-    //            XArray chosenRows = _dictionary.GetChosenRows(_totalRowsRead, _totalRowsRead + outerCount, _totalRowsRead);
+                if (chosenRows.Count > 0)
+                {
+                    CurrentRowCount = chosenRows.Count;
+                    return CurrentRowCount;
+                }
+            }
 
-    //            // Track the total row count (so we know which rows to ask for chosens each time)
-    //            _totalRowsRead += outerCount;
+            return 0;
+        }
 
-    //            // Tell the remapper to filter to chosen rows
-    //            _chosenRowsFilter.SetMatches((int[])chosenRows.Array, chosenRows.Count);
+        private void BuildChooseDictionary()
+        {
+            XArray[] keyarrays = new XArray[_keyColumnGetters.Length];
+            int[] rowIndices = new int[XTableExtensions.DefaultBatchSize];
 
-    //            if (chosenRows.Count > 0) return chosenRows.Count;
-    //        }
+            int totalSoFar = 0;
+            int count;
+            while ((count = _source.Next(XTableExtensions.DefaultBatchSize)) != 0)
+            {
+                // Get the rank and key column arrays
+                XArray rankArray = _rankColumnGetter();
+                for (int i = 0; i < keyarrays.Length; ++i)
+                {
+                    keyarrays[i] = _keyColumnGetters[i]();
+                }
 
-    //        return 0;
-    //    }
+                // Build a row index xarray for these global row IDs
+                for (int i = 0; i < count; ++i)
+                {
+                    rowIndices[i] = totalSoFar + i;
+                }
+                XArray rowIndexArray = XArray.All(rowIndices, count);
+                totalSoFar += count;
 
-    //    private void BuildChooseDictionary()
-    //    {
-    //        XArray[] keyarrays = new XArray[_keyColumnGetters.Length];
-    //        int[] rowIndices = new int[XTableExtensions.DefaultBatchSize];
+                // Add these to the choose dictionary
+                _dictionary.Add(keyarrays, rankArray, rowIndexArray);
+            }
+        }
 
-    //        int totalSoFar = 0;
-    //        int count;
-    //        while ((count = _source.Next(XTableExtensions.DefaultBatchSize)) != 0)
-    //        {
-    //            // Get the rank and key column arrays
-    //            XArray rankxarray = _rankColumnGetter();
-    //            for (int i = 0; i < keyarrays.Length; ++i)
-    //            {
-    //                keyarrays[i] = _keyColumnGetters[i]();
-    //            }
+        public void Reset()
+        {
+            _source.Reset();
+            _totalRowsRead = 0;
+        }
 
-    //            // Build a row index xarray for these global row IDs
-    //            for (int i = 0; i < count; ++i)
-    //            {
-    //                rowIndices[i] = totalSoFar + i;
-    //            }
-    //            XArray rowIndexxarray = XArray.All(rowIndices, count);
-    //            totalSoFar += count;
-
-    //            // Add these to the choose dictionary
-    //            _dictionary.Add(keyarrays, rankxarray, rowIndexxarray);
-    //        }
-    //    }
-
-    //    public void Reset()
-    //    {
-    //        _source.Reset();
-    //        _totalRowsRead = 0;
-    //    }
-
-    //    public void Dispose()
-    //    {
-    //        if (_source != null)
-    //        {
-    //            _source.Dispose();
-    //            _source = null;
-    //        }
-    //    }
-    //}
+        public void Dispose()
+        {
+            if (_source != null)
+            {
+                _source.Dispose();
+                _source = null;
+            }
+        }
+    }
 }
