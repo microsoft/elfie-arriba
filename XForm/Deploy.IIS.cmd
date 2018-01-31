c3ecad1c4150dc14f6592f95f1d2585aa7ea3a6f
Scott Louvau <v-scolo@microsoft.com>
2018-01-31 13:35:35 -0800

    XForm.IIS: Make it easy to copy web.config from elsewhere.

--- XForm/Deploy.IIS.cmd
+++ XForm/Deploy.IIS.cmd
@@ -3,16 +3,25 @@ SET AppCmdPath=%windir%\system32\inetsrv\appcmd.exe
 SET Target=%1
 IF "%Target%"=="" (
   ECHO Error. No destination path specified.
-  ECHO Ex: Deploy.WebSite bin\x64\Release
+  ECHO Usage: Deploy.IIS [TargetPath] [WebConfigSourcePath?]
+  ECHO Ex: Deploy.IIS C:\inetpub\XForm.IIS C:\Configs\XForm.IIS\Web.config
   GOTO :EOF
 )
 
-ECHO - Deploying Website..
-"%AppCmdPath%" stop sites "XForm.IIS"
+SET WebConfigPath=%~dp0XForm.IIS\Web.config
+IF NOT "%2"=="" SET WebConfigPath=%2
+
+ECHO - Deploying Website...
+"%AppCmdPath%" stop apppool "XForm.IIS"
+
 ROBOCOPY /MIR /NJH /NJS "%~dp0XForm.IIS\bin" "%Target%\bin"
 IF NOT %ERRORLEVEL% LEQ 7 GOTO :Error
-COPY /Y "%~dp0XForm.IIS\Web.config" "%Target%\Web.config"
-"%AppCmdPath%" start sites "XForm.IIS"
+
+ECHO - Copying Web.config from [%WebConfigPath%]...
+COPY /Y "%WebConfigPath%" "%Target%\Web.config"
+
+"%AppCmdPath%" start apppool "XForm.IIS"
+
 
 GOTO :End
 :Error
