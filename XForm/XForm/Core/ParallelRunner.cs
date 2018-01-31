fadcd3632d2914eb0de6d47cea100933d7f1d668
Scott Louvau <v-scolo@microsoft.com>
2018-01-31 14:59:57 -0800

    XForm: Add ParallelRunner (easy parallelization within stages)

---
+++ XForm/XForm/Core/ParallelRunner.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace XForm.Core
+{
+    public class ParallelRunner
+    {
+        public static int ParallelCount = Environment.ProcessorCount;
+        
+        public static void Run(int startIndexInclusive, int endIndexExclusive, Action<int, int> method)
+        {
+            if(ParallelCount == 1)
+            {
+                method(startIndexInclusive, endIndexExclusive);
+            }
+
+            int segmentLength = ParallelLengthPart(endIndexExclusive - startIndexInclusive, ParallelCount);
+
+            Parallel.For(0, ParallelCount, (i) =>
+            {
+                int threadStartIndex = startIndexInclusive + i * segmentLength;
+                int threadEndIndex = (i == ParallelCount - 1 ? endIndexExclusive : threadStartIndex + segmentLength);
+
+                method(threadStartIndex, threadEndIndex);
+            });
+        }
+
+        public static int ParallelLengthPart(int totalCount, int parallelCount)
+        {
+            // Divide the count by the number of threads, but make each part (except the last) an even multiple of 64
+            int portionLength = totalCount / parallelCount;
+            if ((portionLength & 63) != 0) portionLength = 64 + portionLength & ~63;
+            return portionLength;
+        }
+    }
+}
