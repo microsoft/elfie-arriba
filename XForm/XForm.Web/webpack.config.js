567a823edcb95134cee051c13bc7fb39fea75c3b
Jeff King <jeffking+github@gmail.com>
2018-02-08 18:49:59 -0800

    Change webpack port to match rootUrl logic.

--- XForm/XForm.Web/webpack.config.js
+++ XForm/XForm.Web/webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
     },
     devServer : {
         host: "0.0.0.0",
-        port: 8081,
+        port: 8080,
         historyApiFallback: {
             rewrites: [
                 { from: /./, to: 'index.html' },
