65ccd4c5a9ed087909819642a4166ae4a09bd268
Scott Louvau <v-scolo@microsoft.com>
2018-03-16 16:34:08 -0700

    Cinco: Added Benchmarker, Ordinal comparison, attempted project reference.   - Cinco now causes Cinco.Native to build, but it doesn't seem to reliably wait for it to finish. Also, the unit tests don't end up with Cinco.Native in the folder where the tests execute.

--- Cinco/Cinco.ConsoleTest/Program.cs
+++ Cinco/Cinco.ConsoleTest/Program.cs
@@ -1,6 +1,5 @@
 ﻿using Cinco.Extensions;
 using System;
-using System.Diagnostics;
 using System.IO;
 
 namespace Cinco.ConsoleTest
@@ -11,35 +10,45 @@ namespace Cinco.ConsoleTest
         {
             return $"{(lengthBytes / 1024.0 / 1024.0):n3} MB";
         }
-
-        public static string ToGigabytesPerSec(this double seconds, long lengthBytes, long iterations)
-        {
-            double bytesPerSecond = (lengthBytes * iterations) / seconds;
-            double gbPerSecond = bytesPerSecond / (1024 * 1024 * 1024);
-            return $"{gbPerSecond:n3} GB/s";
-        }
     }
 
     /// <summary>
     ///  Cinco TODO:
-    ///    - How do I get Cinco reference to copy in Cinco.Native?
-    ///    - Unit Tests
-    ///    - Easy performance comparison run
-    ///    - Add IndexOfAll inside.
+    ///    - Unit Test project won't properly wait for build of Cinco.Native via Cinco reference.
     /// </summary>
     class Program
     {
-        static int IndexOfAll(string text, string value, int[] matches, int fromIndex = 0)
+        public enum Scenario
+        {
+            IndexOf,
+            IndexOfOrdinal,
+            IndexOfN
+        }
+
+        static int MatchCount(Scenario scenario, string text, string value, int fromIndex = 0)
         {
             int matchCount = 0;
+            int index = -1;
 
-            while(matchCount < matches.Length)
+            while(true)
             {
-                int index = text.IndexOf(value, fromIndex);
+                switch (scenario)
+                {
+                    case Scenario.IndexOf:
+                        index = text.IndexOf(value, fromIndex);
+                        break;
+                    case Scenario.IndexOfOrdinal:
+                        index = text.IndexOf(value, fromIndex, StringComparison.Ordinal);
+                        break;
+                    case Scenario.IndexOfN:
+                        index = text.IndexOfN(value, fromIndex);
+                        break;
+                }
+
                 if (index == -1) break;
 
-                matches[matchCount++] = index;
                 fromIndex = index + 1;
+                matchCount++;
             }
 
             return matchCount;
@@ -51,32 +60,16 @@ namespace Cinco.ConsoleTest
             SampleBuilder.ConcatenateCode(@"C:\Code\XForm", sampleFilePath);
 
             string text = File.ReadAllText(sampleFilePath);
-            long lengthBytes = new FileInfo(sampleFilePath).Length;
+            long lengthBytes = (long)new FileInfo(sampleFilePath).Length;
             string value = "IndexOf";
-            int iterations = 100;
 
-            Console.WriteLine($"Searching {iterations:n0} times for '{value}' in '{sampleFilePath}' ({lengthBytes.ToSizeString()})...");
-            Stopwatch w = Stopwatch.StartNew();
-
-            int[] matches = new int[100];
-            int totalCount = 0;
-
-            for (int i = 0; i < iterations; ++i)
+            Console.WriteLine($"Searching for '{value}' in '{sampleFilePath}' ({lengthBytes.ToSizeString()})...");
+            using (Benchmarker b = new Benchmarker("IndexOf", 2000))
             {
-                int fromIndex = 0;
-
-                while (true)
-                {
-                    int matchCount = IndexOfAll(text, value, matches, fromIndex);
-                    totalCount += matchCount;
-
-                    if (matchCount < matches.Length) break;
-                    fromIndex = matches[matchCount - 1] + 1;
-                }
+                b.Measure("String.IndexOf", (int)lengthBytes, () => MatchCount(Scenario.IndexOf, text, value));
+                b.Measure("String.IndexOfOrdinal", (int)lengthBytes, () => MatchCount(Scenario.IndexOfOrdinal, text, value));
+                b.Measure("String.IndexOfN", (int)lengthBytes, () => MatchCount(Scenario.IndexOfN, text, value));
             }
-
-            w.Stop();
-            Console.WriteLine($"Found {totalCount:n0} total matches in {w.ElapsedMilliseconds:n0} ms; {w.Elapsed.TotalSeconds.ToGigabytesPerSec(lengthBytes, iterations)}");
         }
     }
 }
---
+++ Cinco/Cinco.Test/Extensions/String.cs
@@ -0,0 +1,22 @@
+﻿using Cinco.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Cinco.Test.Extensions
+{
+    [TestClass]
+    public class String
+    {
+        [TestMethod]
+        public void String_IndexOfN()
+        {
+            Assert.AreEqual(-1, ((string)null).IndexOfN("."));
+            Assert.AreEqual(-1, ".".IndexOfN(null));
+            Assert.AreEqual(-1, "".IndexOfN("."));
+            Assert.AreEqual(-1, ".".IndexOfN(""));
+
+            Assert.AreEqual(0, ".".IndexOfN("."));
+            Assert.AreEqual(-1, ".".IndexOfN(".", 1));
+            Assert.AreEqual(-1, ".".IndexOfN(".", 1));
+        }
+    }
+}
